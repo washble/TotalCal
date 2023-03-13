e0e86dd7ebe691124feaf146a8bfe8c80be89a39
SANGWOOSEE <washble@devdeopt.co.kr>
2023-03-14 01:38:20 +0900

    Temp commit index.html

---
+++ assets/js/cal.js
@@ -0,0 +1,6 @@
+let message = document.getElementById("message");
+let total = document.getElementById("total");
+let btn_calculation = document.getElementById("btn-calculation");
+btn_calculation.onclick = () => {
+  
+}
